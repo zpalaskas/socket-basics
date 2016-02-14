9a1e188d0657c228facdd97c6b9ddad3a3692187
Zisis Palaskas <zpalaskas@gmail.com>
2016-02-14 12:19:58 +0200

    done the send textbox

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 4000;
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -11,8 +11,10 @@ io.on('connection', function (socket) {
 
     socket.on('message', function (message){
         console.log('Message received : ' + message.text );
+
         socket.broadcast.emit('message', message);
     });
+
     socket.emit('message',{
         text: 'Welcome to the chat application!'
     })
