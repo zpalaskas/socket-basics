c9263b72092c3597aba51584c1a7646fbedbf908
Zisis Palaskas <zpalaskas@gmail.com>
2016-02-14 23:00:24 +0200

    curentusers added

--- public/js/app.js
+++ public/js/app.js
@@ -4,40 +4,40 @@ var socket = io();
 
 console.log(name + ' wants to join ' + room);
 
+// Update h1 tag
 jQuery('.room-title').text(room);
 
 socket.on('connect', function () {
-	console.log('Connected to socket.io server!');
+    console.log('Conncted to socket.io server!');
     socket.emit('joinRoom', {
         name: name,
         room: room
-    })
+    });
 });
 
 socket.on('message', function (message) {
     var momentTimestamp = moment.utc(message.timestamp);
-    var $message = jQuery('.messages')
-	console.log('New message:');
-	console.log(message.text);
+    var $message = jQuery('.messages');
 
-    $message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>' );
+    console.log('New message:');
+    console.log(message.text);
+
+    $message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
     $message.append('<p>' + message.text + '</p>');
-   // jQuery('.messages').append('<p><strong>' + momentTimestamp.local().format('h:mm a') + ': </strong>' + message.text + '</p>');
-    //jQuery('.messages').append('<p>'+ message.text + '</p>');
 });
 
 // Handles submitting of new message
 var $form = jQuery('#message-form');
 
 $form.on('submit', function (event) {
-	event.preventDefault();
+    event.preventDefault();
 
-	var $message = $form.find('input[name=message]');
+    var $message = $form.find('input[name=message]');
 
-	socket.emit('message', {
+    socket.emit('message', {
         name: name,
-		text: $message.val()
-	});
+        text: $message.val()
+    });
 
-	$message.val('');
+    $message.val('');
 });
